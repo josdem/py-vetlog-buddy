8bae760d35b811907996105cd279293f911cdefd
josdem <joseluis.delacruz@gmail.com>
2026-06-06 08:09:26 -0600

    Formatting code

--- tests/unit/test_user_service.py
+++ tests/unit/test_user_service.py
@@ -108,7 +108,9 @@ def test_remove_invalid_logs_removed_count():
     mock_repo.get_all.return_value = [invalid_user, valid_user]
 
     logger_mock = MagicMock()
-    with patch("vetlog_buddy.users.services.Logger", return_value=logger_mock) as LoggerCls:
+    with patch(
+        "vetlog_buddy.users.services.Logger", return_value=logger_mock
+    ) as LoggerCls:
         service = UserService(repo=mock_repo)
         LoggerCls.assert_called_once_with("UserService")
 
